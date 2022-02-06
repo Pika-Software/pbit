990d64fde4a7ae704b6c69f3e5b13583467e1d55
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-02-06 06:44:31 +0400

    Update package.lua

--- lua/gpm/packages/pbit/package.lua
+++ lua/gpm/packages/pbit/package.lua
@@ -2,9 +2,6 @@ return {
     name = "pBit",
     description = "Normal bitwise library without overflowing.",
     version = "1.0.0",
-    dependencies = {
-        ["Random Patches"] = ">=1.0.0"
-    },
     author = "Klen_list",
     license = "MIT"
 }
