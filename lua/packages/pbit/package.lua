84b086d65305f4ec2f5889530fc751d8a8bd9fe3
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-04-25 20:23:49 +0400

    1.0.1

--- lua/packages/pbit/package.lua
+++ lua/packages/pbit/package.lua
@@ -1,7 +1,7 @@
 return {
     name = "pBit",
     description = "Normal bitwise library without overflowing.",
-    version = "1.0.0",
+    version = "1.0.1",
     author = "Klen_list",
     license = "MIT"
 }
